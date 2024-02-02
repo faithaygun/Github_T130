00acf261695611535162b16eaca86706d0251123
fatih aygun <0fatihaygun7@gmil.com>
2024-02-02 17:17:43 +0300

    2. version

---
+++ src/c02_Github.java
@@ -0,0 +1,9 @@
+public class c02_Github {
+    public static void main(String[] args) {
+
+        int a = 12;
+        int b = 8;
+
+        System.out.println("Iki sayinin toplami: " + (a+b));
+    }
+}
